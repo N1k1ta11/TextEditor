e4a2fcad740285f2f89a3548e0e6f1e865a58a8b
nikita <nikitakerzhik@gmail.com>
2023-08-18 18:48:51 +0500

    add translates, textEdit, buttons: save, open, open only read, reference. And working with files

---
+++ TextEditor/QtLanguage_en.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="37"/>
+        <location filename="mainwindow.cpp" line="11"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="50"/>
+        <location filename="mainwindow.cpp" line="10"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <location filename="mainwindow.cpp" line="13"/>
+        <source>Reference</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="76"/>
+        <source>Open only read</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="90"/>
+        <location filename="mainwindow.cpp" line="14"/>
+        <source>ENG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="95"/>
+        <source>RUS</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
